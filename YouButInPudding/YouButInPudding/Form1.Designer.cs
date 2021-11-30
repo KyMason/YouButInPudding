e5f42264280f28591fc9bbd129d4fbdd8638d7a0
Kyleigh Mason <92942831+KyMason@users.noreply.github.com>
2021-11-30 14:12:56 -0600

    Initial Calc Program
    Program can calculate amount of calories in 4 different puddings when tied to a person's weight in kg or lb.

---
+++ YouButInPudding/YouButInPudding/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace YouButInPudding
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter Your Weight";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(189, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(405, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "in";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(484, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(14, 58);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(621, 150);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "THere is a text box here it is hard to see it though";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ YouButInPudding/YouButInPudding/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace YouButInPudding
+{
+    public partial class Form1 : Form
+    {
+        static string[] weights =
+            {"kg","lb"};
+        static string[] puddins =
+            {"Chocolate","Chocolate Sugar Free","Vanilla","Vanilla Sugar Free"};
+        static double UserWeight;
+        static string calories;
+        static string WeighType;
+        static string PuddinType;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(weights);
+            comboBox2.Items.AddRange(puddins);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object WeighTypeSelec = comboBox1.SelectedItem;
+             WeighType = WeighTypeSelec.ToString();
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object PuddinTypeSelec = comboBox2.SelectedItem;
+            PuddinType = PuddinTypeSelec.ToString();
+            
+        }
+
+        private void caloriCalc()
+        {
+            double calMultiplier=1;
+            double calafterCalc;
+            switch (PuddinType)
+            {
+                case "Chocolate":
+                    calMultiplier = 1.087;
+                    break;
+                case "Vanilla":
+                    calMultiplier = 1.087;
+                    break;
+                case "Chocolate Sugar Free":
+                    calMultiplier = .761;
+                    break;
+                case "Vanilla Sugar Free":
+                    calMultiplier = .652;
+                    break;
+                
+            }
+            calafterCalc = calMultiplier * UserWeight;
+            calories = calafterCalc.ToString("F2").TrimEnd('0');
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserWeight = Double.Parse(textBox1.Text);
+            if (WeighType == "lb")
+                UserWeight /= 2.205;
+            UserWeight *= 1000;
+            caloriCalc();
+           
+            richTextBox2.Text = "Calories: " + calories;
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+/* Snack Pack puddings
+ *   Chocolate Sugar Free
+ *      70 Cal for 92g
+ *          ~.761 calories a gram
+ *   Vanilla Sugar Free
+ *      60 Cal for 92g
+ *          ~.652 Calories a gram
+ *   Chocolate
+ *      100 Cal for 92g
+ *          ~1.087 Calories a gram
+ *   Vanilla
+ *      100 Cal for 92g
+ *          ~1.087 Calories a gram
+ */
